17e47cbddef026f60cb0cfc2ebb6e2aff3c9b86e
rsalge <rossnsalge@gmail.com>
2017-11-01 11:58:34 -0500

    implements the react-starter that Patrick provided

---
+++ schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS test;
+
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE player (
+  id int NOT NULL AUTO_INCREMENT,
+  diceRoll integer NOT NULL,
+  victoryPoints integer NOT NULL,
+  settlements integer NOT NULL,
+  cities integer NOT NULL,
+  roadLength integer NOT NULL,
+  knightCount integer NOT NULL
+  PRIMARY KEY (ID)
+);
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
