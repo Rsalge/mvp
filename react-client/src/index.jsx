50ff9106c7907d0c351896296b3f1108409f8289
rsalge <rossnsalge@gmail.com>
2017-11-01 15:49:19 -0500

    playerCount live updates this.state.playerCount, working on hiding player input when start tracking is clicked

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -28,17 +28,37 @@ class App extends React.Component {
 
   handleSubmit(data) {
       console.log('submit data: ', data.target.value);
+      console.log('PLayerCOunt: ', this.state.playerCount);
+      $(".newGame").hide();
+  }
+
+  handlePlayerChange(data) {
+    this.setState({
+      playerCount: data.target.value
+    })
   }
 
   render () {
     return (<div>
       <h1>Settlers Tracker</h1>
-      <input
-        type="number"
-         id="playerCount"
-        //  value={this.state.playerCount}
-         onChange={this.handleSubmit.bind(this)}
-       />
+      <form class="newGame">
+        <label>
+          Number of players
+          <input
+            type="number"
+            min="0"
+            max="7"
+            id="playerCount"
+            value={this.state.playerCount}
+            onChange={this.handlePlayerChange.bind(this)}
+          /><input
+            type="button"
+            id="newGame"
+            value="Start tracking!"
+            onClick={this.handleSubmit.bind(this)}
+          />
+        </label>
+      </form>
       <List turns={this.state.turns}/>
     </div>)
   }
