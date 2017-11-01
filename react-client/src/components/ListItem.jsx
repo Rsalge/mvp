25c88ce61ea0a8566437054fd81b140a4002235b
rsalge <rossnsalge@gmail.com>
2017-11-01 14:37:01 -0500

    adds style sheet

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 //Each list item will be a players scorecard for a specific turn
 const ListItem = (props) => (
   <div>
-    <ul>
-      <li>{ props.turn }</li>
-      <li>{ props.turn }</li>
+    <ul id ="turn">
+      <li class="turnItem">{ props.turn }</li>
+      <li class="turnItem">{ props.turn }</li>
     </ul>
   </div>
 )
