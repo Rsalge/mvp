cd47e49e3fc991a1e7c59d8c71dd27ca1f968be9
rsalge <rossnsalge@gmail.com>
2017-11-02 11:02:30 -0500

    Displaying past turn data, appearing with most recent past turn on bottom, need to reverse the order

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ListItem from './ListItem.jsx';
 //need to either use items property to pass in turns or change to turns
 const List = (props) => (
-  <table>
-    <tbody>
+    <div>
       {console.log('TURN DATA: ', props.turns)}
-
-    </tbody>
-  </table>
+      { props.turns.map(turn => <ListItem turn={turn}/>)}
+    </div>
 )
 
 export default List;
--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 //Each list item will be a players scorecard for a specific turn
-const ListItem = ({turn}) => (
-  <tr>
-    <th>Player Name</th>
-    <th>Dice Roll</th>
-    <th>Victory Points</th>
-    <th>Settlements</th>
-    <th>Cities</th>
-    <th>Road Length</th>
-    <th>Knight Count</th>
-    <th>Turn Number</th>
-    <th>Next Turn</th>
-  </tr>
+const ListItem = (props) => (
 
-  
+  <table>
+    {console.log('CHECKING FOR TURN: ', props.turn)}
+    <tbody>
+      <tr>
+        <th>Player Name</th>
+        <th>Dice Roll</th>
+        <th>Victory Points</th>
+        <th>Settlements</th>
+        <th>Cities</th>
+        <th>Road Length</th>
+        <th>Knight Count</th>
+        <th>Turn Number</th>
+      </tr>
+      <tr>
+        <td>{props.turn.playerName}</td>
+        <td>{props.turn.diceRoll}</td>
+        <td>{props.turn.victoryPoints}</td>
+        <td>{props.turn.settlements}</td>
+        <td>{props.turn.cities}</td>
+        <td>{props.turn.roadLength}</td>
+        <td>{props.turn.knightCount}</td>
+        <td>{props.turn.turn}</td>
+      </tr>
+
+    </tbody>
+  </table>
+
+
+
 )
 
 export default ListItem;
