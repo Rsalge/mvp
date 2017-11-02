86ed52dbb161a54939ae893e888e6994d6dfed7d
rsalge <rossnsalge@gmail.com>
2017-11-01 20:12:03 -0500

    adds handling of changes inside current turn view, links changes to the state

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,13 +8,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      turns: ['JHello','Hi'],
+      turns: [],
       playerCount: 0,
       players: [],
+      currentPlayer: '',
       showForm: true,
       showPlayerInput: false,
       showTurn: false,
-      turn: {playerName:'', diceRoll: 0, victoryPoints: 0, settlements: 0, cities: 0, roadLength:0, knightCount:0, turnNum: 0}
+      playerName:'',
+      diceRoll: 0,
+      victoryPoints: 0,
+      settlements: 0,
+      cities: 0,
+      roadLength:0,
+      knightCount:0,
+      turnNum: 0
     }
   }
 
@@ -50,6 +58,42 @@ class App extends React.Component {
     })
   }
 
+  handleDiceChange(data) {
+    this.setState({
+      diceRoll: data.target.value
+    })
+  }
+
+  handleVictoryPointsChange(data) {
+    this.setState({
+      victoryPoints: data.target.value
+    })
+  }
+
+  handleSettlementsChange(data) {
+    this.setState({
+      settlements: data.target.value
+    })
+  }
+
+  handleCitiesChange(data) {
+    this.setState({
+      cities: data.target.value
+    })
+  }
+
+  handleRoadChange(data) {
+    this.setState({
+      roadLength: data.target.value
+    })
+  }
+
+  handleKnightChange(data) {
+    this.setState({
+      knightCount: data.target.value
+    })
+  }
+
   updatePlayerName(player, data) {
     var newPlayer = this.state.players.slice();
     newPlayer[player] = data.target.value;
@@ -64,9 +108,12 @@ class App extends React.Component {
 
   startTracking(data) {
     console.log('PLAYERS: ', this.state.players);
+    var firstPlayer = this.state.players.slice();
+    firstPlayer = firstPlayer[0];
     this.setState({
       showPlayerInput: !this.state.showPlayerInput,
-      showTurn: !this.state.showTurn
+      showTurn: !this.state.showTurn,
+      currentPlayer: firstPlayer
     })
 
   }
@@ -74,8 +121,10 @@ class App extends React.Component {
   render () {
     const style = { display: this.state.showForm ? 'inline' : 'none' }
     const enterPlayerNames = {display: this.state.showPlayerInput ? 'inline' : 'none' }
+    const turnStyle = {display: this.state.showTurn ? 'inline' : 'none' }
     return (<div>
       <h1>Settlers Tracker</h1>
+
       <form style={style} onSubmit={this.handleSubmit.bind(this)}>
         <label>
           Number of players
@@ -86,7 +135,6 @@ class App extends React.Component {
             id="playerCount"
             value={this.state.playerCount}
             onChange={this.handlePlayerChange.bind(this)}
-            onSubmit={this.handleSubmit.bind(this)}
           /><input
             type="button"
             id="newGame"
@@ -95,6 +143,8 @@ class App extends React.Component {
           />
         </label>
       </form>
+
+
       <form style={enterPlayerNames} onSubmit={this.handleSubmit.bind(this)} >
         <label>
           Enter player names in order, start with who is going first
@@ -113,6 +163,79 @@ class App extends React.Component {
           onClick={this.startTracking.bind(this)}
         />
       </form>
+
+      <table style={turnStyle}>
+        <tbody>
+          <tr>
+            <th>Player Name</th>
+            <th>Dice Roll</th>
+            <th>Victory Points</th>
+            <th>Settlements</th>
+            <th>Cities</th>
+            <th>Road Length</th>
+            <th>Knight Count</th>
+            <th>Turn Number</th>
+          </tr>
+          <tr>
+            <td>{this.state.currentPlayer}</td>
+            <td>
+              <input
+                type="number"
+                min="2"
+                max="12"
+                value={this.state.diceRoll}
+                onChange={this.handleDiceChange.bind(this)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                min="2"
+                value={this.state.victoryPoints}
+                onChange={this.handleVictoryPointsChange.bind(this)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                min="2"
+                max="12"
+                value={this.state.settlements}
+                onChange={this.handleSettlementsChange.bind(this)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                min="2"
+                max="12"
+                value={this.state.cities}
+                onChange={this.handleCitiesChange.bind(this)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                min="2"
+                max="12"
+                value={this.state.roadLength}
+                onChange={this.handleRoadChange.bind(this)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                min="2"
+                max="12"
+                value={this.state.knightCount}
+                onChange={this.handleKnightChange.bind(this)}
+              />
+            </td>
+            <td>{this.state.currentTurn}</td>
+          </tr>
+        </tbody>
+      </table>
+
       <List turns={this.state.turns}/>
     </div>)
   }
